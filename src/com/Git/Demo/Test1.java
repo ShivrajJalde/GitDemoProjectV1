d29369e19017f19ca109d088dff5559a78f36ff5
Shivraj <shivraj902177@gmail.com>
2022-12-22 15:19:52 +0530

    First Commit

---
+++ src/com/Git/Demo/Test1.java
@@ -0,0 +1,14 @@
+package com.Git.Demo;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungry = "no";
+		if (hungry.equals("yes")) {
+			System.out.println("Eat Something");
+
+		} else {
+			System.out.println("Dont Eat");
+		}
+	}
+
+}
